978d6e4f239c46870f7ce9fa839cb3ebaf003d07
Sebastian009w <ia3266595@gmail.com>
2023-04-07 12:19:39 -0500

    se hace el de iniciar session en una cuenta

--- src/controllers/__Auth__.js
+++ src/controllers/__Auth__.js
@@ -22,7 +22,7 @@ export async function register(req, res) {
   }
 
   const user = await new_user.save();
-  
+
   const token = jwt.sign({ id: user._id }, Cg.SECRET, {
     expiresIn: 86400, // 24hrs
   });
@@ -30,6 +30,24 @@ export async function register(req, res) {
   res.status(200).json({ token });
 }
 
-export function login(req, res) {
-  res.send("iniciar session");
+export async function login(req, res) {
+  const found_user = await Username.findOne({ email: req.body.email }).populate(
+    "roles"
+  );
+
+  if (!found_user) return res.status(400).json({ message: "User not found" });
+
+  const bool_user = await Username.compare_password(
+    req.body.password,
+    found_user.password
+  );
+
+  if (!bool_user)
+    return res.status(401).json({ token: null, message: "not authorizado" });
+
+  const token = jwt.sign({ id: found_user._id }, Cg.SECRET, {
+    expiresIn: 86400,
+  });
+
+  res.status(200).json({ token });
 }
